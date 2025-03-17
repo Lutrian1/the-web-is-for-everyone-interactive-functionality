ee11a973080dd109e5d487a7bacf1d94a9f53862
Luuk <147055722+Lutrian1@users.noreply.github.com>
2025-03-17 14:23:01 +0100

    #6: Server met pseudo code
    Pseudo code klaargezet voor de nieuwe post. Ik ga dus de oude verwijderen

--- server.js
+++ server.js
@@ -1,33 +1,21 @@
-// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
-// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
 import express from 'express'
 
-// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
 
-// Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
 
-// Maak werken met data uit formulieren iets prettiger
 app.use(express.urlencoded({extended: true}))
 
-// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
-// Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
 
-// Stel Liquid in als 'view engine'
 const engine = new Liquid();
 app.engine('liquid', engine.express());
 
-// Stel de map met Liquid templates in
-// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set('views', './views')
 
 app.get('/', async function (request, response) {
 
   const milledoniProducts = await fetch('https://fdnd-agency.directus.app/items/milledoni_products')
-
-  // En haal daarvan de JSON op
   const milledoniProductsJSON = await milledoniProducts.json()
    // Render index.liquid uit de Views map
    // Geef hier eventueel data aan mee
@@ -47,10 +35,12 @@ app.get('/gift/:id', async function (request, response) {
 let savedGifts = [];
 
 // POST route om de gift op te slaan
-app.post('/save-gift', express.urlencoded({ extended: true }), async function (request, response) { //express zet het ruwe http format (kan je zien in console van browser) om naar een prettig json object
+app.post('/save-gift', express.urlencoded({ extended: true }), async function (request, response) { //express.urlencoded zet het ruwe http format (kan je zien in console van browser) om naar een prettig json object
     const giftId = request.body.giftId;
 
-    // Fetch gift details van API met giftId
+    //giftID, partial save button
+
+    // Haal de data van de persoon op
     const giftResponse = await fetch(`https://fdnd-agency.directus.app/items/milledoni_products/${giftId}`);
     const giftData = await giftResponse.json();
 
@@ -59,8 +49,18 @@ app.post('/save-gift', express.urlencoded({ extended: true }), async function (r
 
     // Redirect naar homepage (Doe dit voor meerdere pages, voor nu alleen index)
     
-    response.redirect('/');
+    response.redirect(303, '/');
 });
+
+//haalt alle producten op
+
+//check of t product al bestaat
+
+//als het product bestaat verwijder hem
+
+//zo niet voeg hem toe
+
+//redirect naar homepage 
 
 // Route om de likes te laten zien
 app.get('/mysavedgifts', function (request, response) {
@@ -114,13 +114,9 @@ app.post(…, async function (request, response) {
 })
 */
 
-
-// Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
 app.set('port', process.env.PORT || 8000)
 
-// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
-  // Toon een bericht in de console
+
   console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
 })
