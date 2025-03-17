d0582a18376d6efe11c38ec37ab0899a08e7dd0e
Luuk <147055722+Lutrian1@users.noreply.github.com>
2025-03-17 14:49:00 +0100

    #6 : Alles overzichtelijker gemaakt

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+// ----------------------------------------------- TEMPLATING -----------------------------------------------//
+
 import express from 'express'
 
 import { Liquid } from 'liquidjs';
 
 const app = express()
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true})) // Formulierdata parsen
 
 app.use(express.static('public'))
 
@@ -12,6 +14,8 @@ const engine = new Liquid();
 app.engine('liquid', engine.express());
 
 app.set('views', './views')
+
+// ----------------------------------------------- HOMEPAGE  -----------------------------------------------//
 
 app.get('/', async function (request, response) {
 
@@ -21,6 +25,8 @@ app.get('/', async function (request, response) {
    // Geef hier eventueel data aan mee
    response.render('index.liquid' , {allMilledoniProducts: milledoniProductsJSON.data })
 })
+
+// ----------------------------------------------- SPECIFIEKE GIFT PAGE  -----------------------------------------------//
 
 app.get('/gift/:id', async function (request, response) {
 
@@ -32,15 +38,15 @@ app.get('/gift/:id', async function (request, response) {
   
 });
 
+// ----------------------------------------------- SAVE GIFT CODE  -----------------------------------------------//
+// Verander deze code naar de nieuwe dingen dat we hebben geleerd. 
 let savedGifts = [];
 
 // POST route om de gift op te slaan
-app.post('/save-gift', express.urlencoded({ extended: true }), async function (request, response) { //express.urlencoded zet het ruwe http format (kan je zien in console van browser) om naar een prettig json object
+app.post('/save-gift', express.urlencoded({ extended: true }), async function (request, response) { //express zet het ruwe http format (kan je zien in console van browser) om naar een prettig json object
     const giftId = request.body.giftId;
 
-    //giftID, partial save button
-
-    // Haal de data van de persoon op
+    // Fetch gift details van API met giftId
     const giftResponse = await fetch(`https://fdnd-agency.directus.app/items/milledoni_products/${giftId}`);
     const giftData = await giftResponse.json();
 
@@ -49,23 +55,26 @@ app.post('/save-gift', express.urlencoded({ extended: true }), async function (r
 
     // Redirect naar homepage (Doe dit voor meerdere pages, voor nu alleen index)
     
-    response.redirect(303, '/');
+    response.redirect('/');
 });
-
-//haalt alle producten op
-
-//check of t product al bestaat
-
-//als het product bestaat verwijder hem
-
-//zo niet voeg hem toe
-
-//redirect naar homepage 
 
 // Route om de likes te laten zien
 app.get('/mysavedgifts', function (request, response) {
     response.render('mygiftpage.liquid', { savedGifts: savedGifts });
 });
+// Pseudo code POST-route voor de index-URL
+
+//haalt alle producten op
+ 
+ //check of t product al bestaat
+ 
+ //als het product bestaat verwijder hem
+ 
+ //zo niet voeg hem toe
+ 
+ //redirect naar homepage 
+
+// ----------------------------------------------- REDIRECT EN 404 -----------------------------------------------//
 
 // Redirect invalide path van :id gift naar home
 
@@ -78,41 +87,7 @@ app.get('/{*splat}', function (request, response) {
   response.status(404).render('404.liquid');
 }); 
 
-/*
-// Zie https://expressjs.com/en/5x/api.html#app.get.method over app.get()
-app.get(…, async function (request, response) {
-  
-  // Zie https://expressjs.com/en/5x/api.html#res.render over response.render()
-  response.render(…)
-})
-*/
-
-/*
-// Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
-app.post(…, async function (request, response) {
-
-  // In request.body zitten alle formuliervelden die een `name` attribuut hebben in je HTML
-  console.log(request.body)
-
-  // Via een fetch() naar Directus vullen we nieuwe gegevens in
-
-  // Zie https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch over fetch()
-  // Zie https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify over JSON.stringify()
-  // Zie https://docs.directus.io/reference/items.html#create-an-item over het toevoegen van gegevens in Directus
-  // Zie https://docs.directus.io/reference/items.html#update-an-item over het veranderen van gegevens in Directus
-  await fetch(…, {
-    method: …,
-    body: JSON.stringify(…),
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
-    }
-  });
-
-  // Redirect de gebruiker daarna naar een logische volgende stap
-  // Zie https://expressjs.com/en/5x/api.html#res.redirect over response.redirect()
-  response.redirect(303, …)
-})
-*/
+// ----------------------------------------------- PORT -----------------------------------------------//
 
 app.set('port', process.env.PORT || 8000)
 
